d5080ee9db49eddbfe3fca9a422055b2bc898a75
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2015-08-10 16:13:33 +0300

    Warn about the electron process not being found in his PATH during panda installation

---
+++ Build.pm
@@ -0,0 +1,18 @@
+use v6;
+
+use Panda::Builder;
+
+class Build is Panda::Builder {
+    method build($workdir) {
+      # The electron process is not found, help the module user a bit :)
+      use File::Which;
+      unless which('electron').defined {
+        say q{
+  Electron seems to be not installed. Please follow the instructions in
+  https://github.com/azawawi/perl6-electron/blob/master/README.md
+  
+  Thanks for using this module :)
+        };
+      }
+    }
+}
